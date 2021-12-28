d5e7fe15b04ff7b5dd73e61f138c6414de363a70
franco <x.francoruiz@gmail.com>
2021-12-28 17:24:51 -0300

    using data and methods

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // Initialize vuejs
 const app = Vue.createApp({
-  template: `<h2>Vuejs</h2>`,
+  // template: `<h2>Vuejs</h2>`,
+  data() {
+    return {
+      title: "Using VueJS with data",
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Using vueJS methods";
+      this.capitalize();
+    },
+    capitalize() {
+      this.title = this.title.toUpperCase();
+    },
+  },
 });
 
-app.mount('#myApp');
+app.mount("#myApp");
